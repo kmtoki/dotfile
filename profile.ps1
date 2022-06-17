ad7651f403d6358056e7a5de306c67e6b9169e92
kmtoki <higumaido@gmail.com>
2022-06-17 17:33:06 +0900

    powershell profile. set utf8

--- profile.ps1
+++ profile.ps1
@@ -3,8 +3,32 @@ Set-PSReadlineKeyHandler -Key Ctrl+v -Function Paste
 Set-PSReadlineKeyHandler -Key Ctrl+p -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key Ctrl+n -Function HistorySearchForward
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-Set-Alias v gvim
 
 function prompt {
   "`e[34m$($pwd.path)`e[0m`n> "
+}
+
+Set-Alias v gvim
+Set-Alias e gvim
+Set-Alias chrome Start-Chrome
+
+chcp 65001 >> 0
+[System.Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
+[System.Console]::InputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
+$env:LESSCHARSET = "utf-8"
+
+function Start-Chrome() {
+  start chrome
+}
+
+function prompt {
+  "`e[34m$($pwd.path)`e[0m`n> "
+}
+
+function bin($n) {
+  return [convert]::ToString($n,2)
+}
+
+function hex($n) {
+  return [convert]::ToString($n,16)
 }
