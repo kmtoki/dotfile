edc83dd292aa870460aae984d313eb563af9d742
kmtoki <higumaido@gmail.com>
2022-12-25 20:13:37 +0900

    update++

--- profile.ps1
+++ profile.ps1
@@ -1,30 +1,25 @@
+chcp 65001 > nul
+[System.Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
+[System.Console]::InputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
+$env:LESSCHARSET = "utf-8"
+
 Set-PSReadLineOption -EditMode Emacs -BellStyle None
 Set-PSReadlineKeyHandler -Key Ctrl+v -Function Paste
 Set-PSReadlineKeyHandler -Key Ctrl+p -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key Ctrl+n -Function HistorySearchForward
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
-Set-Alias e nvim
-Set-Alias v goneovim
-Set-Alias c Start-Chrome
-
-chcp 65001 >> 0
-[System.Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
-[System.Console]::InputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
-$env:LESSCHARSET = "utf-8"
+function prompt {
+  "`e[34m$($pwd.path)`e[0m`n> "
+}
 
 function Start-Chrome() {
   start chrome
 }
 
-function prompt {
-  "`e[34m$($pwd.path)`e[0m`n> "
-}
+Set-Alias v C:\Users\higum\scoop\apps\goneovim\current\goneovim.exe
+Set-Alias c Start-Chrome
 
-function bin($n) {
-  return [convert]::ToString($n,2)
-}
-
-function hex($n) {
-  return [convert]::ToString($n,16)
-}
+#Install-Module Get-ChildItemColor
+Import-Module Get-ChildItemColor
+Set-Alias ls Get-ChildItemColor
